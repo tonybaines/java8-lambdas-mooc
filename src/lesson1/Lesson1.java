a1ad9509a7e60d5f3b4b1752eb10abb08f12974e
Tony Baines <tony.baines.1@gmail.com>
2015-07-15 15:32:08 +0100

    use a method reference rather than anonymous lambda

--- src/lesson1/Lesson1.java
+++ src/lesson1/Lesson1.java
@@ -111,7 +111,7 @@ public class Lesson1 {
   private void exercise5() {
     List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-    new Thread(() -> list.forEach((i) -> System.out.println(i))).start();
+    new Thread(() -> list.forEach(System.out::println)).start();
   }
 
   /**
